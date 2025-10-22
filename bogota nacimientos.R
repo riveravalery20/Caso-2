bb27a652f9062ee783b3520810c0dea79f7adb06
riveravalery20 <rivera.valery@correounivalle.edu.co>
2025-10-21 21:59:29 -0500

    base filtrada 2do semestre del año y en bogota

--- bogota nacimientos.R
+++ bogota nacimientos.R
@@ -1,11 +1,15 @@
 library(readr)
 library(dplyr)
 
+# Cargamos base y visualizamos
+
 BD_EEVV_Nacimientos_2024 <- read_csv("BD-EEVV-Nacimientos-2024.csv")
-#seleccion de variables de 
+View(BD_EEVV_Nacimientos_2024)
+
+# Selección y renombrado de variables
 
 Nacimientos <- BD_EEVV_Nacimientos_2024 %>%
-  select(COD_DPTO, PESO_NAC, T_GES, TIPO_PARTO, NUMCONSUL, EDAD_MADRE, N_EMB) %>%
+  select(COD_DPTO, PESO_NAC, T_GES, TIPO_PARTO, NUMCONSUL, EDAD_MADRE, N_EMB, MES) %>%
   rename(
     Departamento = COD_DPTO,
     Peso = PESO_NAC,
@@ -13,35 +17,47 @@ Nacimientos <- BD_EEVV_Nacimientos_2024 %>%
     Tipo_parto = TIPO_PARTO,
     Numero_control_prenatal = NUMCONSUL,
     Edad_madre = EDAD_MADRE,
-    Numero_embarazos = N_EMB
+    Numero_embarazos = N_EMB,
+    Mes = MES
+  )
+# 3. Convertir variables a numéricas (importante antes de cualquier cálculo)
+Nacimientos <- Nacimientos %>%
+  mutate(
+    Tiempo_gestación = as.numeric(Tiempo_gestación),
+    Tipo_parto = as.numeric(Tipo_parto),
+    Numero_control_prenatal = as.numeric(Numero_control_prenatal),
+    Edad_madre = as.numeric(Edad_madre),
+    Numero_embarazos = as.numeric(Numero_embarazos),
   )
 
-# 4. Clasificar el peso como Moderado o Delicado
+# Clasificar el peso
 
 Nacimientos <- Nacimientos %>%
   mutate(
-    Peso = ifelse(
-      Peso %in% c(5, 6, 7, 8, 9),
-      "Moderado",
-      "Delicado"
-    )
+    Peso = ifelse(Peso %in% c(5, 6, 7, 8, 9), "Moderado", "Delicado")
   )
 
+# Filtrar solo segundo semestre
+Nacimientos_SegundoSemestre <- Nacimientos %>%
+  mutate(Mes = as.numeric(Mes)) %>%
+  filter(Mes %in% c(7, 8, 9, 10, 11, 12))
+View(Nacimientos_SegundoSemestre)
 
-#proporción de Bogotá en los datos totales
-
-Proporcion_Bogota <- Nacimientos %>%
+# Proporción de Bogotá en el segundo semestre
+Proporcion_Bogota_SegundoSemestre <- Nacimientos_SegundoSemestre %>%
   count(Departamento) %>%
   mutate(Proporcion = n / sum(n)) %>%
   filter(Departamento == 11)
 
-print(Proporcion_Bogota)
+print(Proporcion_Bogota_SegundoSemestre)
 
-# Base final de nacimientos en bogota 
-
-Nacimientos_Bogota <- Nacimientos %>%
-  filter(Departamento == 11) %>%   # 11 = Bogotá
+# Base final de nacimientos en Bogotá 2024 segundo semestre
+Nacimientos_Bogota <- Nacimientos_SegundoSemestre %>%
+  filter(Departamento == 11) %>%
   mutate(Departamento = "Bogotá")
-
 View(Nacimientos_Bogota)
+
+# Proporción interna de pesos en Bogotá
 prop.table(table(Nacimientos_Bogota$Peso)) * 100
+
+
