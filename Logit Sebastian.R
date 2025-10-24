15e729d12981bbb38f85e70850f6f8eb917026e4
sebastianaldana0 <joan.aguirre@correounivalle.edu.co>
2025-10-23 19:05:27 -0500

    cambios

--- Logit Sebastian.R
+++ Logit Sebastian.R
@@ -3,6 +3,8 @@
 library(readr)
 library(dplyr)
 library(tidyverse)
+library(caret)
+library(ISLR)
 
 #Base general
 
@@ -22,27 +24,27 @@ BD_EEVV <- read_csv("BD-EEVV-Nacimientos-2024.csv") %>%
     Tiempo_gestación = as.numeric(Tiempo_gestación), 
     Tipo_parto = as.numeric(Tipo_parto),
     Numero_control_prenatal = as.numeric(Numero_control_prenatal), 
-    Edad_madre = as.numeric(Edad_madre), 
     Numero_embarazos = as.numeric(Numero_embarazos), 
   ) %>% 
   mutate(
-    Peso = ifelse(Peso %in% c(5, 6, 7, 8, 9), "Moderado", "Delicado")
+    Peso = ifelse(Peso %in% c(5, 6, 7, 8, 9), "No", "Si")
   ) %>% 
   mutate(Mes = as.numeric(Mes)) %>%
   filter(Mes %in% c(7, 8, 9, 10, 11, 12)) %>% 
   filter(Departamento == 11) %>%
-  mutate(Departamento = "Bogotá")
+  mutate(Departamento = "Bogotá") %>% 
+  na.omit()
 
 #Balanceo
 set.seed(5)
 
-Delicados <- BD_EEVV %>% filter(Peso == "Delicado")
-Moderados <- BD_EEVV %>% filter(Peso == "Moderado") %>% 
+Si <- BD_EEVV %>% filter(Peso == "Si")
+No <- BD_EEVV %>% filter(Peso == "No") %>% 
   sample_n(6000)
 
 #Base final
 
-Base_datos <- bind_rows(Delicados, Moderados) %>% 
+Base_datos <- bind_rows(Si, No) %>% 
   select(Tiempo_gestación,Tipo_parto,Numero_control_prenatal,Edad_madre,Numero_embarazos,
          Peso) %>% mutate(Tipo_parto=case_when(
            Tipo_parto == 1 ~ "Espontaneo",
@@ -59,7 +61,8 @@ Base_datos <- bind_rows(Delicados, Moderados) %>%
     Edad_madre == 7 ~ "40-44 años",
     Edad_madre == 8 ~ "45-49 años",
     Edad_madre == 9 ~ "50-54 años"
-  ))
+  )) %>% 
+  filter(complete.cases(.))
 
 #KNN
 
@@ -68,3 +71,21 @@ index_muestra <- sample(11873,11873)
 index_entrena <- sample(11873,8904)
 
 index_test <- index_muestra[!index_muestra %in% index_entrena]
+
+BD_entrena <- Base_datos[index_entrena, ]
+
+BD_test <- Base_datos[index_test, ]
+
+BD_entrena_input <- BD_entrena[, 1:5]
+BD_entrena_output <- BD_entrena[, 6]
+
+BD_test_input <- BD_test[, -6]
+BD_test_output <- BD_test[, 6]
+
+#K optimo
+
+BD_knnEntrenado <- train(Peso ~ ., 
+                         data = BD_entrena, 
+                         method = "knn",  
+                         tuneLength = 200
+)
